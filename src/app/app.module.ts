a8b1f77d7080d3fcd503566ed89e4dd9257414c3
Daniel Alsadiq <95635439+Berserker8008@users.noreply.github.com>
2023-11-29 22:47:25 +0300

    Header WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { SearchbarComponent } from './header/searchbar/searchbar.component';
+import { StudentCardComponent } from './main/student-card/student-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    NavbarComponent,
+    SearchbarComponent,
+    StudentCardComponent
   ],
   imports: [
     BrowserModule,
