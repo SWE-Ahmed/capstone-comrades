9c2a8867fc57b28b964bc309b522f9aab410368f
HassanAli232 <ha-ali-al@hotmail.com>
2023-12-04 05:10:02 +0300

    finished edit-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { AccountInfoComponent } from './account-info/account-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    EditProfileComponent
+    EditProfileComponent,
+    AccountInfoComponent
   ],
   imports: [
     BrowserModule,
