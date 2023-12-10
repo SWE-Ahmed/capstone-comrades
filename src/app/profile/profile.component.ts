bc6f3424a380ef53754807b2a94c6d336f4c0df0
HassanAli232 <ha-ali-al@hotmail.com>
2023-12-10 22:07:14 +0300

    prepared functionality of profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,8 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  showChangePasswordButton = true;
+  showEditButton = true;
+  showCancelButton = false;
+  showSubmitButton = false;
 
+  isEditMode = false;
+
+  onChangePassword() {
+    // Change functionality.
+  }
+
+  onEdit() {
+    this.moveToEditMode();
+  }
+
+  onSubmit() {
+    // ToDo: Perform submit logic here
+    // For now, just toggling the button visibility
+
+    this.exitEditMode();
+  }
+
+  onCancel() {
+    // ToDo: Handle cancel logic here
+
+    // For now, just toggling the button visibility
+    this.exitEditMode();
+  }
+
+  moveToEditMode() {
+    this.isEditMode = true;
+
+    this.showChangePasswordButton = false;
+    this.showEditButton = false;
+    this.showCancelButton = true;
+    this.showSubmitButton = true;
+  }
+
+  exitEditMode() {
+    this.isEditMode = false;
+
+    this.showChangePasswordButton = true;
+    this.showEditButton = true;
+    this.showCancelButton = false;
+    this.showSubmitButton = false;
+  }
 }
