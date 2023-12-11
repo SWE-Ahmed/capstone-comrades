d9b97082398060cd95483fc5b967474abee4e036
Daniel Alsadiq <95635439+Berserker8008@users.noreply.github.com>
2023-12-11 20:57:32 +0300

    Added change password pop-up

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  @ViewChild('newpass') newPasswordInput!: ElementRef;
+  @ViewChild('confirmPass') confirmPassInput!: ElementRef;
 
+  showEyeNewPass: boolean = false;
+  showEyeConfirmPass: boolean = false;
+  showPasswordNewPass: boolean = false;
+  showPasswordConfirmPass: boolean = false;
+
+  togglePasswordVisibility(inputType: 'newpass' | 'confirmPass'): void {
+    if (inputType === 'newpass') {
+      this.showEyeNewPass = !this.showEyeNewPass;
+      this.showPasswordNewPass = !this.showPasswordNewPass;
+      this.newPasswordInput.nativeElement.type = this.showPasswordNewPass ? 'text' : 'password';
+    } else if (inputType === 'confirmPass') {
+      this.showEyeConfirmPass = !this.showEyeConfirmPass;
+      this.showPasswordConfirmPass = !this.showPasswordConfirmPass;
+      this.confirmPassInput.nativeElement.type = this.showPasswordConfirmPass ? 'text' : 'password';
+    }
+  }
 }
