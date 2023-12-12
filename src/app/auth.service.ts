120235b7a5978be55e6e87e5db81ac16a90f474a
ABDULMOHSEEN <aam20122003@gmail.com>
2023-12-12 18:02:09 +0300

    adding the validation functions for the signup page, BUT, the last page which is the password page is not implimented well

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
-import {sendEmailVerification} from 'firebase/auth'
+import { sendEmailVerification } from 'firebase/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private afAuth: AngularFireAuth, private router: Router) { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
   private authenticated: boolean = false;
 
   // sign up method for new users
   signUp(email: string, password: string): void {
-    this.afAuth.createUserWithEmailAndPassword(email, password)
+    console.log(email);
+    this.afAuth
+      .createUserWithEmailAndPassword(email, password)
       .then(() => {
         // Sign up successful
-        this.router.navigate([''])
+        this.router.navigate(['']);
       })
       .catch((error) => {
         // An error occurred
@@ -24,35 +25,36 @@ export class AuthService {
   }
   // sign in method for existing users
   signIn(email: string, password: string): void {
-    this.afAuth.signInWithEmailAndPassword(email, password)
+    this.afAuth
+      .signInWithEmailAndPassword(email, password)
       .then(() => {
         // Login successful
         this.authenticated = true;
-        this.router.navigate(['profile'])
+        this.router.navigate(['profile']);
       })
       .catch((error) => {
         // An error occurred
-        console.log(error)
+        console.log(error);
       });
   }
   // sign out method
-  signOut(){
-    this.afAuth.signOut()
-    .then(() => {
-      // Sign Out successful
-      this.authenticated = false;
-      this.router.navigate(['']);
-    })
-    .catch((error) => {
-      // An error occured
-      console.log(error)
-    });
-}
+  signOut() {
+    this.afAuth
+      .signOut()
+      .then(() => {
+        // Sign Out successful
+        this.authenticated = false;
+        this.router.navigate(['']);
+      })
+      .catch((error) => {
+        // An error occured
+        console.log(error);
+      });
+  }
   // get authentication status
   isAuthenticated(): boolean {
     return this.authenticated;
   }
   // create profile document
-  createProfile(accountDetails: any): void {
-  }
+  createProfile(accountDetails: any): void {}
 }
--- src/app/signup-submit/signup-submit.component.ts
+++ src/app/signup-submit/signup-submit.component.ts
@@ -1,29 +1,41 @@
-import { Component, EventEmitter, Injectable, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Injectable,
+  Input,
+  Output,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup-submit',
   templateUrl: './signup-submit.component.html',
-  styleUrls: ['./signup-submit.component.css']
+  styleUrls: ['./signup-submit.component.css'],
 })
 export class SignupSubmitComponent {
-
-  constructor(private router: Router, private auth: AuthService) {
-  }
+  constructor(private router: Router, private auth: AuthService) {}
 
   @Input() showFirstToggle: boolean = false;
-  @Output() updateView = new EventEmitter <boolean> ();
+  @Output() updateView = new EventEmitter<boolean>();
 
   popup: boolean = false;
 
-  closePopup(email: string, password:string): void {
+  closePopup(email: string, password: string): void {
     this.popup = false;
-    console.log(email, password)
-    this.auth.signUp(email, password)
+    console.log(email, password);
+    this.auth.signUp(email, password);
   }
 
   goBack(): void {
-    this.updateView.emit(!this.showFirstToggle)
+    this.updateView.emit(!this.showFirstToggle);
+  }
+
+  checkPassword(password: string, passwordConfirm: string) {
+    if (password === passwordConfirm) {
+      this.popup = true;
+    } else {
+      //TODO: show an erro message for the user about the problem and ask him to fix
+    }
   }
 }
