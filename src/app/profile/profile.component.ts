0e215d0f266584624d2ae8bb087a06d44ec6f6db
SWE-Ahmed <master.necro15@gmail.com>
2023-12-12 19:57:07 +0300

    push mergeV4

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,75 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+ 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
-  constructor(private auth: AuthService) {
+  showChangePasswordButton = true;
+  showEditButton = true;
+  showCancelButton = false;
+  showSubmitButton = false;
+ 
+  isEditMode = false;
+ 
+  onChangePassword() {
+    // Change functionality.
   }
-  signOut(): void {
-    this.auth.signOut();
+ 
+  onEdit() {
+    this.moveToEditMode();
+  }
+ 
+  onSubmit() {
+    // ToDo: Perform submit logic here
+  }
+ 
+  onCancel() {
+    // ToDo: Handle cancel logic here
+ 
+    // For now, just toggling the button visibility
+    this.exitEditMode();
+  }
+ 
+  moveToEditMode() {
+    this.isEditMode = true;
+ 
+    this.showChangePasswordButton = false;
+    this.showEditButton = false;
+    this.showCancelButton = true;
+    this.showSubmitButton = true;
+  }
+ 
+  exitEditMode() {
+    this.isEditMode = false;
+ 
+    this.showChangePasswordButton = true;
+    this.showEditButton = true;
+    this.showCancelButton = false;
+    this.showSubmitButton = false;
+  }
+ 
+ 
+  @ViewChild('newpass') newPasswordInput!: ElementRef;
+  @ViewChild('confirmPass') confirmPassInput!: ElementRef;
+ 
+  showEyeNewPass: boolean = false;
+  showEyeConfirmPass: boolean = false;
+  showPasswordNewPass: boolean = false;
+  showPasswordConfirmPass: boolean = false;
+ 
+  togglePasswordVisibility(inputType: 'newpass' | 'confirmPass'): void {
+    if (inputType === 'newpass') {
+      this.showEyeNewPass = !this.showEyeNewPass;
+      this.showPasswordNewPass = !this.showPasswordNewPass;
+      this.newPasswordInput.nativeElement.type = this.showPasswordNewPass ? 'text' : 'password';
+    } else if (inputType === 'confirmPass') {
+      this.showEyeConfirmPass = !this.showEyeConfirmPass;
+      this.showPasswordConfirmPass = !this.showPasswordConfirmPass;
+      this.confirmPassInput.nativeElement.type = this.showPasswordConfirmPass ? 'text' : 'password';
+    }
   }
 }
+ 
+ 
