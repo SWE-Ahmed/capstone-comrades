c1751ba15a27d071bd9c026457396eb688ed1f32
Daniel Alsadiq <95635439+Berserker8008@users.noreply.github.com>
2023-12-14 20:01:59 +0300

    Created responsive pages for login signup signup confirmation

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-signin',
@@ -8,8 +9,33 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
-  constructor(private router: Router, private auth: AuthService) {
+  isLargeScreen: boolean = true;  // Default to true for larger screens
+
+  constructor(private router: Router, private auth: AuthService,private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() { // Checks if the screen size is small or Xsmall and changes the boolean variable
+    this.breakpointObserver.observe([
+      Breakpoints.Small,
+      Breakpoints.XSmall
+    ]).subscribe(result => {
+      this.isLargeScreen = !result.matches;
+    });
   }
+
+  @ViewChild('passwordValue') PasswordInput!: ElementRef; // Password input
+
+  showPass: boolean = false;
+  showEye: boolean = false;
+
+  togglePasswordVisibility(): void {
+    
+      this.showEye = !this.showEye;
+      this.showPass = !this.showPass;
+      this.PasswordInput.nativeElement.type = this.showPass
+        ? 'text'
+        : 'password';
+    }
+
 
   launchProfile(email: string, password: string): void {
     this.auth.signIn(email, password)
